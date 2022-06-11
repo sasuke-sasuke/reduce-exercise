da81d5fef624ff3a008bb6ec05084b7a350889d8
Jeof Petty <jeofpetty@icloud.com>
2022-06-10 20:19:40 -0400

    add extractValue()

--- reduce.js
+++ reduce.js
@@ -6,7 +6,14 @@ Examples:
     extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractValue(arr, key) {}
+function extractValue(arr, key) {
+    const names = [];
+    arr.reduce(function(accum, nextVal){
+        names.push(nextVal[key])
+        return nextVal;
+    }, names)
+    return names;
+}
 
 /*
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
@@ -19,7 +26,8 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+function vowelCount(str) {
+}
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
